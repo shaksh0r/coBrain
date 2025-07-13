369b37ae30fb358f281f123a0953016b6b89aea5
shakshor <shakshor123@gmail.com>
2025-07-13 17:47:02 +0600

    basic backend setup

---
+++ backend/coBrain/src/main/java/com/therap/coBrain/CoBrainApplication.java
@@ -0,0 +1,13 @@
+package com.therap.coBrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoBrainApplication.class, args);
+	}
+
+}
---
+++ backend/coBrain/src/test/java/com/therap/coBrain/CoBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.therap.coBrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
