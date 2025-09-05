07b4f65a129ff9568488650df4124fbd9d0d0a03
shakshor <shakshor123@gmail.com>
2025-09-06 00:48:16 +0600

    done testing

---
+++ backend/demo/src/main/java/com/test/database/demo/Entities/User.java
@@ -0,0 +1,28 @@
+package com.test.database.demo.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data // Generates getters, setters, equals, hashCode, and toString methods
+@NoArgsConstructor // Generates a no-argument constructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
